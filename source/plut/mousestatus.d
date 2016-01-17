6a0ef2d10f7abb80b48ecdfc1b4f86c38787f57f
Anatoly Kalin aka ttldtor <ttldtor@gmail.com>
2016-01-17 17:31:21 +0300

    D syntax

--- source/plut/mousestatus.d
+++ source/plut/mousestatus.d
@@ -2,6 +2,6 @@ module plut.mousestatus;
 
 struct MouseSatus {
     int x, y; // absolute (column, row) 0 based, measured in characters
-    short button[3]; // state of each button
+    short[3] button; // state of each button
     int changes; // flags indicating what has changed with the mouse
 };

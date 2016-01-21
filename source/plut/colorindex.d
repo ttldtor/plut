6d3d88ce228996f5f486fbfc8b42b1652aa8d6c1
Anatoly Kalin aka ttldtor <ttldtor@gmail.com>
2016-01-22 01:28:34 +0300

    lighter/darker

--- source/plut/colorindex.d
+++ source/plut/colorindex.d
@@ -28,12 +28,29 @@ version(Windows) {
         Default      = 256
     };
 
-    ColorIndex light(ColorIndex c) {
-        if (c != ColorIndex.Default) {
+    ColorIndex lighter(ColorIndex c) {
+        if (c < ColorIndex.Gray) {
+            return cast(ColorIndex)(c ^ ColorIndex.Bright);
+        } else if (c == ColorIndex.Gray) {
+            return ColorIndex.LightGray;
+        }
+
+        return c;
+    }
+
+    ColorIndex darker(ColorIndex c) {
+        if (c == ColorIndex.LightGray) {
+            return ColorIndex.Gray;
+        } else if (c >= ColorIndex.Gray && c < ColorIndex.Default) {
             return cast(ColorIndex)(c ^ ColorIndex.Bright);
         }
 
         return c;
+    }
+
+    unittest {
+        assert(ColorIndex.Black.lighter.lighter.lighter == ColorIndex.White);
+        assert(ColorIndex.Red.lighter.darker == ColorIndex.Red);
     }
 
 } else version(Posix) {

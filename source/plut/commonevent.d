5a82a253def5540620c5921afaee8c61e8df37f7
Anatoly Kalin aka ttldtor <ttldtor@gmail.com>
2016-01-23 12:49:03 +0300

    ValueChangeEvent

--- source/plut/commonevent.d
+++ source/plut/commonevent.d
@@ -1,7 +1,13 @@
 ﻿module plut.commonevent;
 
+import az.core.az;
+
 struct CommonEvent(ElementType, ValueType) {
     ElementType element;
     ValueType oldValue;
     ValueType newValue;
+};
+
+template ValueChangeEvent(ValueType) {
+    alias ValueChangeEvent = CommonEvent!(Az, ValueType);
 };

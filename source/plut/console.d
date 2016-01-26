5440c68fece7d79a166bb85421f5321e3ebeb3e0
Anatoly Kalin aka ttldtor <ttldtor@gmail.com>
2016-01-26 23:35:27 +0300

    console + handlers

--- source/plut/console.d
+++ source/plut/console.d
@@ -1,9 +1,45 @@
 module plut.console;
 
-import plut.colorindex;
+import az.core.az;
+import az.core.handler;
 
-class Console {
+import plut.colorindex;
+import plut.sizeevent;
+import plut.mouseevent;
+import plut.keyboardevent;
+
+class Console: Az {
+    
+private:
+    
     ColorIndex defaultForegroundColor_;
     ColorIndex defaultBackgroundColor_;
     
+    auto keyboardEventsHandler_ = new SharedHandler!(Az /+ sender +/, KeyboardEvent /+ event +/);
+    auto mouseEventsHandler_ = new SharedHandler!(Az /+ sender +/, MouseEvent /+ event +/);
+    auto sizeEventsHandler_ = new SharedHandler!(Az /+ sender +/, SizeEvent /+ event +/);
+    
+public:
+
+    @property {
+        ColorIndex defaultForegroundColor() {
+            return defaultForegroundColor_;
+        }
+        
+        ColorIndex defaultBackgroundColor() {
+            return defaultBackgroundColor_;
+        }
+        
+        auto keyboardEventsHandler() {
+            return keyboardEventsHandler_;
+        }
+        
+        auto mouseEventsHandler() {
+            return mouseEventsHandler_;
+        }
+        
+        auto sizeEventsHandler() {
+            return sizeEventsHandler_;
+        }
+    }
 };

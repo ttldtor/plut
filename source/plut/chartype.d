cad696266dd3b10b97ff167f1e4fbbde67a4ef66
Anatoly Kalin aka ttldtor <ttldtor@gmail.com>
2016-01-28 23:46:00 +0300

    some char types + char type -> WinAPI CHAR_INFO

--- source/plut/chartype.d
+++ source/plut/chartype.d
@@ -2,8 +2,26 @@
 
 import plut.colorindex;
 
-struct CharType {
+struct CommonCharType(C) {
     ColorIndex foreground;
     ColorIndex background;
-    char data;
+    C data;
 };
+
+alias CharType = CommonCharType!char;
+alias WideCharType = CommonCharType!wchar;
+alias DoubleWideCharType = CommonCharType!dchar;
+
+version(Windows) {
+    import core.sys.windows.windows;
+    import std.utf;
+    
+    CHAR_INFO toCharInfo(C)(const(C) c) if(is(C : CharType) || is(C : WideCharType) || is(C : DoubleWideCharType)) {
+        CHAR_INFO result;
+        
+        result.Attributes = c.foreground | c.background;
+        result.UnicodeChar = ("" ~ c.data).toUTF16()[0];
+        
+        return result;
+    }
+}

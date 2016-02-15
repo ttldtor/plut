e38a13f8f579fc3aef5e7881167a4d65431b5348
ttldtor <ttldtor@gmail.com>
2016-02-15 21:05:37 +0300

    Size.toString

--- source/plut/size.d
+++ source/plut/size.d
@@ -1,7 +1,13 @@
 module plut.size;
 
+import std.format;
+
 struct CommonSize(N) {
     N width, height;
 };
 
 alias Size = CommonSize!int;
+
+string toString(Size s) {
+    return format("(%s x %s)", s.width, s.height);
+}
